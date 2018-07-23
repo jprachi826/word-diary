e1b0b9e5ecb0aa3c0ce6b427bc4ec09642402c56
Prachi Jain <jprachi826@gmail.com>
2018-07-24 00:32:12 +0530

    Add files via upload

---
+++ MainActivity/src/com/project/wordiary/database/Entry.java
@@ -0,0 +1,18 @@
+package com.project.wordiary.database;
+
+
+public class Entry {
+
+    public static final String TABLE_NAME = "entries";
+
+    public static final String _ID = "_id";
+
+    public static final String COLUMN_NAME_DAY_ID = "day_id";
+
+    public static final String COLUMN_NAME_MESSAGE = "message";
+
+    public static final String COLUMN_NAME_MOOD = "mood";
+
+    public static final String COLUMN_NAME_CREATED = "created";
+
+}
